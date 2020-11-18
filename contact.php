c11753a0756ed4997b096873e6f3e19d66c62c99
Tigist Degai <66580266+TigistDegai@users.noreply.github.com>
2020-11-18 16:16:06 -0600

    Add files via upload
    commit

--- contact.php
+++ contact.php
@@ -1,25 +1,27 @@
 <?php 
-require 'db_config.php';
+include "db_config.php";
 
   //creating connection to database
-$con=mysqli_connect('localhost','root','','reservation') or die(mysqli_error());
-  
+$conn=mysqli_connect('localhost','root','','reservation') or die(mysqli_error());
+
 //check whether submit button is pressed or not
 if((isset($_POST['submit'])))
 {
   //fetching and storing the form data in variables
-$Name = $con->real_escape_string($_POST['name']);
-$Email = $con->real_escape_string($_POST['email']);
-$Phone = $con->real_escape_string($_POST['contact']);
-$comments = $con->real_escape_string($_POST['text']);
+$Name = $conn->real_escape_string($_POST['name']);
+$Email = $conn->real_escape_string($_POST['email']);
+$Phone = $conn->real_escape_string($_POST['contact']);
+$comments = $conn->real_escape_string($_POST['text']);
   //query to insert the variable data into the database
 $sql="INSERT INTO contactus (name, email, phone, comments) VALUES ('".$Name."','".$Email."', '".$Phone."', '".$comments."')";
-  //Execute the query and returning a message
-if(!$result = $con->query($sql)){
+
+//Execute the query and returning a message
+if(!$result = $conn->query($sql)){
 die('Error occured [' . $conn->error . ']');
 }
 else
-   echo "Thank you! We will get in touch with you soon";
+
+  echo "<h2> Thank you! We will get in touch with you soon </h2>";
 }
 ?>
 
@@ -48,36 +50,39 @@ else
         <center>
             <div class="center backg">
                 <div>
-                <h2>Contact Us</h2>
-  
+                <h2>Quick Contact</h2>
+                <h4>contact us today and get reply with in 24 hours </h4>
   <form class="form" action="contact.php" method="POST">
-    
+
     <p class="username">
+    <label for="name">Name</label>
       <input type="text" name="name" id="name" placeholder="Enter your name" >
-      <label for="name">Name</label>
+     
     </p>
-    
+
     <p class="useremail">
-      <input type="text" name="email" id="email" placeholder="mail@example.com" >
-      <label for="email">Email</label>
+    <label for="email">Email</label>
+      <input type="text" name= "email" id= "email" placeholder="mail@example.com" >
+      
     </p>
-    
+
     <p class="usercontact">
-      <input type="text" name="contact" id="contact" placeholder="contact no." >
       <label for="contact">Phone number</label>
+      <input type="text" name="contact" id="contact" placeholder="contact no." >
+      
     </p>    
-  
+
     <p class="usertext">
-      <textarea name="text" placeholder="Write something to us" ></textarea>
-                        <label for="text">Comments</label>
+      <label for="text">Comments</label>
+      <textarea name="text" placeholder="Write something to us" ></textarea>             
     </p>
-    
+
     <p class="usersubmit">
       <input type="submit" name="submit" value="Send" >
     </p>
   </form>
-                    
+
         </center>
     </body>
 	<!-- // content -->
-    </html> 
+    </html>   
